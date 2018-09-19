caee582bbfbe016cd55790efc4a08806a0195211
Ryan <hiRyan@protonmail.com>
2018-09-19 21:21:33 +0300

    Singleton: singleton created and is many times faster. However, there's debate in the developers world whether Singletons should ever be used.

--- src/main/java/ee/sda/patterns/creational/singleton/Main.java
+++ src/main/java/ee/sda/patterns/creational/singleton/Main.java
@@ -2,11 +2,14 @@ package ee.sda.patterns.creational.singleton;
 
 public class Main {
 
-    // after refactoring it still takes some time
+    // now with the single instance it runs even faster
     public static void main(String[] args) {
         VeryExpensiveService veryExpensiveService1 = VeryExpensiveService.getInstance();
         VeryExpensiveService veryExpensiveService2 = VeryExpensiveService.getInstance();
         VeryExpensiveService veryExpensiveService3 = VeryExpensiveService.getInstance();
         VeryExpensiveService veryExpensiveService4 = VeryExpensiveService.getInstance();
+        VeryExpensiveService veryExpensiveService5 = VeryExpensiveService.getInstance();
+        VeryExpensiveService veryExpensiveService6 = VeryExpensiveService.getInstance();
+        VeryExpensiveService veryExpensiveService7 = VeryExpensiveService.getInstance();
     }
 }
--- src/main/java/ee/sda/patterns/creational/singleton/VeryExpensiveService.java
+++ src/main/java/ee/sda/patterns/creational/singleton/VeryExpensiveService.java
@@ -3,8 +3,10 @@ package ee.sda.patterns.creational.singleton;
 public class VeryExpensiveService {
 
 
-
     // simulation of resource heavy service
+
+    private static VeryExpensiveService instance;
+
 
     // changed to private, for factory method
     private VeryExpensiveService() {
@@ -12,12 +14,20 @@ public class VeryExpensiveService {
     }
 
     // factory method
-    public static VeryExpensiveService getInstance(){
-        try {
-            Thread.sleep (2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public static VeryExpensiveService getInstance() {
+        // if service is not initiated...
+        if (instance == null) {
+
+            // simulation of expensive creation
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // singleton instance
+            instance = new VeryExpensiveService();
         }
-        return new VeryExpensiveService();
+        return instance;
     }
 }
