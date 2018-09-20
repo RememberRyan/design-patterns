bdae56ac3ba15c36e23e316fdda875bf88a90425
Ryan <hiRyan@protonmail.com>
2018-09-20 21:22:42 +0300

    Flyweight design pattern, demonstrating how things can be up to x5 faster. This is often used in game development to create many, grass, clouds, trees etc. You create only one tree and then you can draw the same tree in different places. In memory you have one tree but in the 3d model you have many of them.

--- src/main/java/ee/sda/patterns/structural/flyweight/WindowsFlyweightFactory.java
+++ src/main/java/ee/sda/patterns/structural/flyweight/WindowsFlyweightFactory.java
@@ -1,5 +1,23 @@
 package ee.sda.patterns.structural.flyweight;
 
+import ee.sda.patterns.structural.flyweight.model.Window;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class WindowsFlyweightFactory {
 
+    // implementation of HashMap
+    private Map<String, Window> cache = new HashMap<>();
+
+
+    public Window createWindow(String title) {
+        if (cache.containsKey(title)) {
+            return cache.get(title);
+        }
+
+        Window window = new Window(title);
+        cache.put(title, window);
+        return window;
+    }
 }
