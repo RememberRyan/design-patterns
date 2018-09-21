365587fdd5bc8239d24cfd610e6744a1f8b6964c
Ryan <hiRyan@protonmail.com>
2018-09-21 18:59:34 +0300

    Refactoring of code before refactoring and inverting the 'if logic' withing Auth.RequestFilter

--- src/main/java/ee/sda/patterns/behavioral/chainofresponsibility/AuthenticationRequestFilter.java
+++ src/main/java/ee/sda/patterns/behavioral/chainofresponsibility/AuthenticationRequestFilter.java
@@ -4,19 +4,23 @@ import ee.sda.patterns.behavioral.chainofresponsibility.model.Request;
 
 public class AuthenticationRequestFilter implements RequestFilter {
 
-
+    // constant value now changed to be FINAL
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String VALID_AUTHORIZATION_VALUE = "Bearer 133314124";
     private RequestFilter nextFilter;
 
     @Override
     public void handle(Request request) {
-        String authorization = request.getHeaders().get("Authorization");
-        if (authorization != null) {
-            if (authorization.equals("Bearer 133314124")) {
+        String authorization = request.getHeaders().get(AUTHORIZATION_HEADER);
+        // merging previous two if statements, further refactoring
+        if (authorization != null
+            &&
+            // remove magic string by extracting it to a constant, also
+             authorization.equals(VALID_AUTHORIZATION_VALUE)) {
                 if (nextFilter != null) {
                     nextFilter.handle(request);
                 }
             }
-        }
 
         throw new UnsupportedOperationException("request is missing correct authorization header");
     }
--- src/main/java/ee/sda/patterns/behavioral/chainofresponsibility/exception/UnauthorizedException.java
+++ src/main/java/ee/sda/patterns/behavioral/chainofresponsibility/exception/UnauthorizedException.java
@@ -3,6 +3,6 @@ package ee.sda.patterns.behavioral.chainofresponsibility.exception;
 public class UnauthorizedException extends RuntimeException {
 
     public UnauthorizedException (String message) {
-        super(message);
+        super(message); // super is used to call constructor from runtime exception
     }
 }
