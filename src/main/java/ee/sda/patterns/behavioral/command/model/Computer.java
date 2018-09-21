3ec95db3e7b74c5ab3e3df45d892c3736d5d6700
Ryan <hiRyan@protonmail.com>
2018-09-21 19:32:15 +0300

    Computer is all setup. Now going to create the ComputerCommand

--- src/main/java/ee/sda/patterns/behavioral/command/model/Computer.java
+++ src/main/java/ee/sda/patterns/behavioral/command/model/Computer.java
@@ -3,4 +3,35 @@ package ee.sda.patterns.behavioral.command.model;
 public class Computer {
 
     private ComputerState state;
+
+    public Computer() {
+        this.state = ComputerState.OFF;
+    }
+
+    public boolean powerOn(){
+        this.state = ComputerState.ON;
+        return true;
+    }
+
+    public boolean powerOff(){
+        this.state = ComputerState.OFF;
+        return true;
+    }
+
+    public boolean doCalculation(String expression) {
+        this.state =ComputerState.BUSY;
+        calculate(expression);
+        this.state = ComputerState.ON;
+        return true;
+    }
+
+    private void calculate (String expression) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
