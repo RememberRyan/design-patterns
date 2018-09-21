0a44731084acda8a4f7dce1545704b73ec4092d7
Ryan <hiRyan@protonmail.com>
2018-09-21 19:18:46 +0300

    Gave first configured framework to our web server (more readable)

--- src/main/java/ee/sda/patterns/behavioral/chainofresponsibility/Main.java
+++ src/main/java/ee/sda/patterns/behavioral/chainofresponsibility/Main.java
@@ -7,14 +7,16 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        // first filter
         AuthenticationRequestFilter authenticationRequestFilter
                 = new AuthenticationRequestFilter();
 
+        // second filter
         AuditLogRequestFilter auditLogRequestFilter = new AuditLogRequestFilter();
 
         auditLogRequestFilter.nextFilter(authenticationRequestFilter);
 
-        // give first configured framework to our web server
+        // Test data: give first configured framework to our web server
         String url = "http://example.com";
         Map<String, String> headers = new HashMap<>();
         headers.put("Accept", "application/json");
